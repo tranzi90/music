8ef389fd61b4469c397318e62a4477bd470faa34
denis_levchenko <tranzi90@gmail.com>
2023-01-12 21:26:00 +0300

    extract header component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
-    <header id="header" class="bg-gray-700">
-        <nav
-            class="container mx-auto flex justify-start items-center py-5 px-4"
-        >
-            <!-- App Name -->
-            <a class="text-white font-bold uppercase text-2xl mr-4" href="#"
-                >Music</a
-            >
-
-            <div class="flex flex-grow items-center">
-                <!-- Primary Navigation -->
-                <ul class="flex flex-row mt-1">
-                    <!-- Navigation Links -->
-                    <li>
-                        <a class="px-2 text-white" href="#">Login / Register</a>
-                    </li>
-                    <li>
-                        <a class="px-2 text-white" href="#">Manage</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    <AppHeader />
 
     <!-- Introduction -->
     <section class="mb-8 py-20 text-white text-center relative">
@@ -476,3 +454,6 @@
         </div>
     </div>
 </template>
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
